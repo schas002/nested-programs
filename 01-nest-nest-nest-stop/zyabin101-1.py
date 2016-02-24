32773174cf1fe660da8c46c6e42d8b93cb1eb831
schas002 <schas002@users.noreply.github.com>
2016-02-24 14:24:34 +0300

    Upload solution zyabin101-1 to task 1

---
+++ 01-nest-nest-nest-stop/zyabin101-1.py
@@ -0,0 +1,7 @@
+# Python > Ruby, 2 languages, 20 bytes
+# 34567890123456789012345678901234567890123456789012345678901234
+
+print 'puts "STOP!"'
+
+# Full list of supported languages:
+# Python Ruby (2)
