6f5bf11645f5d2675d0fa403e2ee597518dc69b0
schas002 <schas002@users.noreply.github.com>
2016-04-26 20:47:34 +0300

    Amend solution zyabin101-1 to task 1

--- 01-nest-nest-nest-stop/zyabin101-1.py
+++ 01-nest-nest-nest-stop/zyabin101-1.py
@@ -1,7 +1,7 @@
-# Python > Ruby, 2 languages, 20 bytes
+# Python > Bash, 3 languages, 29 bytes
 # --5---10----5---20----5---30----5---40----5---50----5---60--64
 
-print 'puts "STOP!"'
+print 'puts "echo \'STOP!\'"'
 
 # Full list of supported languages:
-# Python Ruby (2)
+# Python Ruby Bash (3)
