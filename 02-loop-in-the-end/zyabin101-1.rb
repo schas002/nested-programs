315ec8f7959ac6e54556dc7c7c3d20c93b9cd595
schas002 <schas002@users.noreply.github.com>
2016-04-30 11:19:53 +0300

    Upload solution zyabin101-1 to task 2

---
+++ 02-loop-in-the-end/zyabin101-1.rb
@@ -0,0 +1,7 @@
+# Ruby > Python, 2 languages, 45 bytes
+# --5---10----5---20----5---30----5---40----5---50----5---60--64
+
+puts "s = 's = %r\\nprint(s%%s)'\nprint(s%s)"
+
+# Full list of supported languages:
+# Ruby Python (2)
